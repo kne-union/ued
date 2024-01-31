d82345e535d9161ce157f24df1e6af6bbbbddb63
zhipenglin <sunandmoon001@163.com>
2024-01-31 11:01:06 +0800

    Initialize project using Create React App

---
+++ craco.config.js
@@ -0,0 +1,19 @@
+const CracoModuleFederation = require("@kne/craco-module-federation");
+const {CracoReadmePlugin} = require("@kne/modules-dev");
+const aliasConfig = require("./webstorm.webpack.config");
+
+module.exports = {
+    webpack: {
+        alias: aliasConfig.resolve.alias, configure: (webpackConfig) => {
+            const definePlugin = webpackConfig.plugins.find((plugin) => plugin.constructor.name === "DefinePlugin");
+            Object.assign(definePlugin.definitions["process.env"], {
+                DEFAULT_VERSION: `"${process.env.npm_package_version}"`
+            });
+            return webpackConfig;
+        }
+    }, plugins: [{
+        plugin: CracoReadmePlugin
+    }, {
+        plugin: CracoModuleFederation
+    }]
+};
---
+++ src/preset.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {preset as fetchPreset} from '@kne/react-fetch';
+import {Spin, Empty} from 'antd';
+import axios from 'axios';
+import {preset as remoteLoaderPreset} from '@kne/remote-loader';
+
+window.PUBLIC_URL = process.env.PUBLIC_URL;
+
+const componentsCoreRemote = {
+    remote: "components-core",
+    url: "https://registry.npmmirror.com",
+    tpl: "{{url}}/@kne%2f{{remote}}/{{version}}/files/build",
+    defaultVersion: '0.1.3',
+};
+
+remoteLoaderPreset({
+    remotes: {
+        default: componentsCoreRemote,
+        'components-core': componentsCoreRemote,
+        'components-name': process.env.NODE_ENV === 'development' ? {
+            remote: 'components-name', url: '/', tpl: '{{url}}'
+        } : {
+            remote: 'components-name',
+            url: 'https://registry.npmmirror.com',
+            tpl: '{{url}}/@kne%2f{{remote}}/{{version}}/files/build',
+            defaultVersion: process.env.DEFAULT_VERSION
+        }
+    }
+});
+
+export const ajax = axios.create({
+    validateStatus: function () {
+        return true;
+    }
+});
+
+
+fetchPreset({
+    ajax,
+    loading: <Spin delay={500}
+                   style={{position: 'absolute', left: '50%', padding: '10px', transform: 'translateX(-50%)'}}/>,
+    error: null,
+    empty: <Empty/>,
+    transformResponse: (response) => {
+        const {data} = response;
+        response.data = {
+            code: data.code === 0 ? 200 : data.code, msg: data.msg, results: data.data
+        };
+        return response;
+    }
+});
