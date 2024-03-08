b3b7173a7918aa009e30d2689471cc791f7b8934
zhipenglin <sunandmoon001@163.com>
2024-03-08 15:53:42 +0800

    修改部署地址

--- src/setupProxy.js
+++
@@ -1,8 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const remoteComponentsMock = require('../public/remote-components.mock.json');
-
-module.exports = function (app) {
-  app.use('/ued-manifest', (req, res) => {
-    return res.json(remoteComponentsMock);
-  });
-};
